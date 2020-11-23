313f63a4787bbeb3b8bd59a909ea567ab0cb579b
e <313665199@qq.com>
2020-11-23 10:33:06 +0800

    vue todolist

--- document5(ui)/scripts/Plug - ins.js
+++ document5(ui)/scripts/Plug - ins.js
@@ -28,102 +28,131 @@
             _this.click(function(){     
                 $("#mask").css("height","100%");   
                 $("#mask").css("width","100%");               
-                $(".dialog").addClass("dialogshow");     
-                $(".dialog").css("position","fixed");
+                $(".dialog").removeClass("dialogclose").addClass("dialogshow");     
+                $(".dialog").css({"position":"fixed","display":"block"});
             })
             $("#btn1").click(function(){
                 $("#mask").css("height",0);   
                 $("#mask").css("width",0);
-                $(".dialog").removeClass("dialogshow");
+                $(".dialog").addClass("dialogclose");
+                setTimeout(function () {
+                    $(".dialog").css("display","none")
+                }, 1000);
+                
             })
             $("#btn2").click(function(){
                 $("#mask").css("height",0);   
                 $("#mask").css("width",0);      
-                $(".dialog").removeClass("dialogshow");        
+                $(".dialog").addClass("dialogclose");
+                setTimeout(function () {
+                    $(".dialog").css("display","none")
+                }, 1000);        
             })
         });
         return this;
     }
-    $.fn.slider = function(options){
+    // $.fn.slider = function(options){
+    //     var defaults = {
+    //         //各种参数，各种属性
+    //     }
+    //     var options = $.extend(defaults,options);
+    //     this.each(function(){
+    //         //各种功能  //可以理解成功能代码
+    //         $(function(){
+    //     var num = 0;
+    //     var timer=null;
+    //     go();
+    //     $(".slider").mouseenter(function(){//实现当鼠标移动到box上时，不执行自动轮播功能
+    //         clearInterval(timer);//移动进去时清除定时器
+    //     }).mouseleave(function(){
+    //         go();//移出后执行go函数
+    //     });
+
+    //     $(".spanlist").eq(0).css("backgroundColor"," gray");
+    //     function go(){
+    //         timer=setInterval(function(){
+    //         num++;
+    //         if(num>4){
+    //             num=1;
+    //             $(".images").css("margin-left","0px")
+    //         }
+    //         if(num==4){
+    //             $(".spanlist").css("backgroundColor","white")
+    //             $(".spanlist").eq(0).css("backgroundColor","gray");
+    //         }
+    //         else{
+    //             $(".spanlist").css("backgroundColor","white");
+    //             $(".spanlist").eq(num).css("backgroundColor","gray");
+    //         }
+    //         $(".images").animate({"marginLeft":-600*num+"px"},580);//使用animate实行运动功能
+            
+    //         },2000);
+    //     };
+    //     $(".spanlist").each(function(index){
+    //         $(this).click(function(){
+    //             num=index;
+    //             $(".spanlist").css("backgroundColor","white");
+    //             $(".spanlist").eq(num).css("backgroundColor","gray");
+    //             $(".images").animate({"marginLeft":-600*num+"px"},580);
+    //         })
+    //     })
+    //     $(".btn-right").click(function(){
+    //             if(num<=4){
+    //                 num++;
+    //             }
+    //             if(num>4){
+    //                 num=1;
+    //                 $(".images").css("margin-left","0px");
+    //             }
+    //             if(num==4){
+    //                 $(".spanlist").css("backgroundColor","white");
+    //                 $(".spanlist").eq(0).css("backgroundColor","gray");
+    //             }
+    //             else{
+    //                 $(".spanlist").css("backgroundColor","white");
+    //                 $(".spanlist").eq(num).css("backgroundColor","gray");
+    //             }
+    //             $(".images").stop().animate({"marginLeft":-600*num+"px"},200);
+    
+    //         });
+    //         $(".btn-left").click(function(){
+    //             if(num>=0){num--;}
+    //             if(num<0){
+    //                 num=3;
+    //                 $(".images").css("margin-left","-2400px")
+    //                 $(".spanlist").css("backgroundColor","white");
+    //                 $(".spanlist").eq(3).css("backgroundColor","gray");
+    //             }
+    //             else{
+    //                 $(".spanlist").css("backgroundColor","white");
+    //                 $(".spanlist").eq(num).css("backgroundColor","gray");
+    //             }
+    //             $(".images").stop().animate({Left:"250px"},200);
+    //         });
+    //     })
+    // })
+    // }
+    $.fn.odivnav = function(options){
         var defaults = {
             //各种参数，各种属性
         }
         var options = $.extend(defaults,options);
         this.each(function(){
             //各种功能  //可以理解成功能代码
-            $(function(){
-        var num = 0;
-        var timer=null;
-        go();
-        $(".slider").mouseenter(function(){//实现当鼠标移动到box上时，不执行自动轮播功能
-            clearInterval(timer);//移动进去时清除定时器
-        }).mouseleave(function(){
-            go();//移出后执行go函数
-        });
-
-        $(".spanlist").eq(0).css("backgroundColor"," gray");
-        function go(){
-            timer=setInterval(function(){
-            num++;
-            if(num>4){
-                num=1;
-                $(".images").css("margin-left","0px")
-            }
-            if(num==4){
-                $(".spanlist").css("backgroundColor","white")
-                $(".spanlist").eq(0).css("backgroundColor","gray");
-            }
-            else{
-                $(".spanlist").css("backgroundColor","white");
-                $(".spanlist").eq(num).css("backgroundColor","gray");
-            }
-            $(".images").animate({"marginLeft":-600*num+"px"},580);//使用animate实行运动功能
-            
-            },2000);
-        };
-        $(".spanlist").each(function(index){
-            $(this).click(function(){
-                num=index;
-                $(".spanlist").css("backgroundColor","white");
-                $(".spanlist").eq(num).css("backgroundColor","gray");
-                $(".images").animate({"marginLeft":-600*num+"px"},580);
+            $("#sl1").click(function(){                
+                $(".slider ul").removeClass("animateClass").css("margin-left","0px")
             })
-        })
-        $(".btn-right").click(function(){
-                if(num<=4){
-                    num++;
-                }
-                if(num>4){
-                    num=1;
-                    $(".images").css("margin-left","0px");
-                }
-                if(num==4){
-                    $(".spanlist").css("backgroundColor","white");
-                    $(".spanlist").eq(0).css("backgroundColor","gray");
-                }
-                else{
-                    $(".spanlist").css("backgroundColor","white");
-                    $(".spanlist").eq(num).css("backgroundColor","gray");
-                }
-                $(".images").stop().animate({"marginLeft":-600*num+"px"},200);
-    
-            });
-            $(".btn-left").click(function(){
-                if(num>=0){num--;}
-                if(num<0){
-                    num=3;
-                    $(".images").css("margin-left","-2400px")
-                    $(".spanlist").css("backgroundColor","white");
-                    $(".spanlist").eq(3).css("backgroundColor","gray");
-                }
-                else{
-                    $(".spanlist").css("backgroundColor","white");
-                    $(".spanlist").eq(num).css("backgroundColor","gray");
-                }
-                $(".images").stop().animate({Left:"250px"},200);
-            });
-        })
-    })
+            $("#sl2").click(function(){
+                $(".slider ul").removeClass("animateClass").css("margin-left","-600px")
+            })
+            $("#sl3").click(function(){
+                $(".slider ul").removeClass("animateClass").css("margin-left","-1200px")
+            })
+            $("#sl4").click(function(){
+                $(".slider ul").removeClass("animateClass").css("margin-left","-1800px")
+            })
+        });
+        
     }
     $.fn.select = function(options){
     var defaults = {
@@ -139,6 +168,7 @@
             }else{
                 $('.select .city').addClass('hide');
             }
+            
         })
         $('.select ul li').on('click',function(){
             console.log($(this).html(), '$(this).html()');
